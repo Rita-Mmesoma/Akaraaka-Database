772a9c3fd0f91bfdd8fe4534d16c84fc8d8ad9dc
Rita Mmesoma <ritammesoma357@gmail.com>
2025-11-12 06:44:16 +0100

    updated app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }))
 //Handle preflight requests
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
