2d1dfad856816a8909a7ec02201807c9568b98dc
Rita Mmesoma <ritammesoma357@gmail.com>
2026-02-04 00:20:27 +0100

    imported the controller correctly

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,4 +1,4 @@
-const User = require('../models/User'); // Adjust path to your User model
+const User = require('../models/User');
 
 exports.getAllUsers = async (req, res, next) => {
     try {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { protect, adminOnly } = require('../middleware/auth');
-const userController = require('../controllers/user.controller');
+const userController = require('../controllers/user.controllers');
 
 router.get('/', protect, adminOnly, userController.getAllUsers);
 
